c9e0b6c3750e08ad853603f39b14af7fe3b4f5fe
Darragh Ferry <darraghferry@hotmail.com>
2020-01-17 08:40:22 +0000

    added post projects endpoint

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -14,4 +14,14 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  Projects.insert(req.body)
+    .then(newProject => {
+      res.status(200).json(newProject);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong" });
+    });
+});
+
 module.exports = router;
