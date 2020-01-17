b1bdfcff65bfd0663b37f430c1454a9c56729966
Darragh Ferry <darraghferry@hotmail.com>
2020-01-17 09:58:40 +0000

    added some endpoints for /api/actions

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -14,4 +14,34 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  Actions.insert(req.body)
+    .then(newAction => {
+      res.status(200).json(newAction);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Actions.update(req.params.id, req.body)
+    .then(updated => {
+      res.status(200).json(updated);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Actions.remove(req.params.id)
+    .then(deleted => {
+      res.status(200).json(deleted);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong" });
+    });
+});
+
 module.exports = router;
