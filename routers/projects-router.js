31af2901843eb7c343b7356c48c0aca52b01deca
Darragh Ferry <darraghferry@hotmail.com>
2020-01-17 08:56:48 +0000

    put endpoint created

--- routers/projects-router.js
+++ routers/projects-router.js
@@ -14,14 +14,37 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
-  Projects.insert(req.body)
+router.post("/", validateProect, (req, res) => {
+  Projects.insert(req.user)
     .then(newProject => {
-      res.status(200).json(newProject);
+      res.status(201).json(newProject);
     })
     .catch(err => {
       res.status(500).json({ message: "something went wrong" });
     });
 });
+
+router.put("/:id", validateProect, (req, res) => {
+  Projects.update(req.params.id, req.user)
+    .then(updated => {
+      res.status(200).json(updated);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong" });
+    });
+});
+
+//middleware
+function validateProect(req, res, next) {
+  console.log("inside validateProject");
+  if (Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "missing project data" });
+  } else if (!req.body.name || !req.body.description) {
+    res.status(400).json({ message: "both name and description required" });
+  } else {
+    req.user = req.body;
+    next();
+  }
+}
 
 module.exports = router;
